c3f30ee9996e128dfd5d8993690ca25ca4e067ec
Dineshkumarpatra <dineshpatra205@gmail.com>
2026-07-04 20:00:26 +0530

    feat: update LED blink interval to 500ms

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,19 +1,11 @@
-/*
- * Project : LED Blink
- * Author  : Dinesh Patra
- * Date    : 04-07-2026
- * Description:
- * Blink the built-in LED every 1 second.
- */
-
 void setup() {
   pinMode(13, OUTPUT);
 }
 
 void loop() {
   digitalWrite(13, HIGH);
-  delay(1000);
+  delay(500);
 
   digitalWrite(13, LOW);
-  delay(1000);
+  delay(500);
 }
