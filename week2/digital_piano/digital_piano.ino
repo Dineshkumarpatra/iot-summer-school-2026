07e4b8b9623d171c27b7cfbb4ba1084ff6a52be6
Dineshkumarpatra <dineshpatra205@gmail.com>
2026-07-04 23:19:56 +0530

    feat: add chord detection

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,3 +1,25 @@
+/*
+ * Project : Digital Piano
+ * Author  : Dinesh Patra
+ * Date    : 04-07-2026
+ */
+
+const int buzzer = 8;
+
+const int btnDo = A0;
+const int btnRe = A1;
+const int btnMi = A2;
+const int btnFa = A3;
+
+void setup() {
+
+  pinMode(btnDo, INPUT);
+  pinMode(btnRe, INPUT);
+  pinMode(btnMi, INPUT);
+  pinMode(btnFa, INPUT);
+
+  pinMode(buzzer, OUTPUT);
+}
 void loop() {
 
   bool doKey = digitalRead(btnDo);
